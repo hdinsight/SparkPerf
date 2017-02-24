877e3d17ebddf7c1f37a4a6cc1886e3ee31c59b2
Nan Zhu <zhunansjtu@gmail.com>
2017-02-24 12:03:32 -0800

    add datamover

---
+++ src/main/scala/com/databricks/spark/sql/perf/DataMover.scala
@@ -0,0 +1,20 @@
+package com.databricks.spark.sql.perf
+
+import com.databricks.spark.sql.perf.queries.tpcds.Tables
+
+import org.apache.spark.sql.SparkSession
+
+object DataMover {
+
+  def main(args: Array[String]): Unit = {
+    val srcPath = args(0)
+    val dstPath = args(1)
+    val sparkSession = SparkSession.builder().getOrCreate()
+    val dummyTableObj = new Tables(sparkSession.sqlContext, "", 1)
+    val tables = dummyTableObj.tables
+    for ((tableName, _) <- tables.map(_.name)) {
+      val df = sparkSession.read.parquet(srcPath + "/" + tableName)
+      df.write.parquet(dstPath)
+    }
+  }
+}
