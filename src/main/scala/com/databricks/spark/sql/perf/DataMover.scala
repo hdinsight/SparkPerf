a553025d82511aedb7ad821f6d0e7adcd005d72f
Nan Zhu <zhunansjtu@gmail.com>
2017-02-24 12:13:30 -0800

    add s3 credentials

--- src/main/scala/com/databricks/spark/sql/perf/DataMover.scala
+++ src/main/scala/com/databricks/spark/sql/perf/DataMover.scala
@@ -11,6 +11,14 @@ object DataMover {
     val dstPath = args(1)
     val sparkSession = SparkSession.builder().getOrCreate()
     val dummyTableObj = new Tables(sparkSession.sqlContext, "", 1)
+
+    sparkSession.sparkContext.hadoopConfiguration.set("fs.s3a.impl",
+      "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    sparkSession.sparkContext.hadoopConfiguration.set("fs.s3a.access.key",
+      args(2))
+    sparkSession.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key",
+      args(3))
+
     val tables = dummyTableObj.tables
     for (tableName <- tables.map(_.name)) {
       val df = sparkSession.read.parquet(srcPath + "/" + tableName)
